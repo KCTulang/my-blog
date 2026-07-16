3c7d798a1d766ea4b0d7ccd05eb14544578772bd
KC <kctulang10@gmail.com>
2026-07-16 10:54:01 +0800

    fix(config): add allowedOrigins to next config to resolve vercel server action errors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	cacheComponents: true,
+	experimental: {
+		serverActions: {
+			allowedOrigins: [
+				"localhost:3000",
+				"*.vercel.app",
+			],
+		},
+	},
 };
 
 export default nextConfig;
